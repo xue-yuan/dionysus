54ab3634978835dd423a1b23e7920829682190c5
Ares <denon@xueyuan.dev>
2024-06-09 23:29:16 -0400

    Replace magic number with constants

--- backend/utils/responses.py
+++ backend/utils/responses.py
@@ -1,6 +1,6 @@
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, Response
-from starlette.status import HTTP_204_NO_CONTENT
+from starlette.status import HTTP_204_NO_CONTENT, HTTP_200_OK
 
 
 class EmptyResponse(Response):
@@ -12,7 +12,12 @@ class ObjectResponse(JSONResponse):
     media_type = "application/json"
 
     def __init__(
-        self, content, status_code=200, headers=None, media_type=None, background=None
+        self,
+        content,
+        status_code=HTTP_200_OK,
+        headers=None,
+        media_type=None,
+        background=None,
     ):
         super().__init__(
             jsonable_encoder(content), status_code, headers, media_type, background
